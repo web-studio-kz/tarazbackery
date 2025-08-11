8b2d908ceb52284881f3471d429e696557d772e0
Nickolay1986 <kolya-grek@list.ru>
2025-08-11 16:37:55 +0500

    fix

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,23 +1,21 @@
+// src/components/AppRouter.jsx (ВРЕМЕННАЯ ОТЛАДОЧНАЯ ВЕРСИЯ)
 
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux'; 
-import { authRoutes, publicRoutes } from '../routes'; 
-import { MENU_ROUTE } from '../utils/consts'; 
+import { publicRoutes } from '../routes'; // У нас сейчас все роуты публичные для теста
+import { MENU_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
-    const { isAuth } = useSelector(state => state.user); 
+    console.log("AppRouter рендерится. Список роутов:", publicRoutes);
 
     return (
         <Routes>
-            {isAuth && authRoutes.map(({ path, Component }) =>
-                <Route key={path} path={path} element={<Component />} />
-            )}
-
+            {/* Рендерим ВСЕ роуты, которые есть в publicRoutes */}
             {publicRoutes.map(({ path, Component }) =>
                 <Route key={path} path={path} element={<Component />} />
             )}
 
+            {/* Если ничего не подошло, перенаправляем на главную */}
             <Route path="*" element={<Navigate to={MENU_ROUTE} replace />} />
         </Routes>
     );
