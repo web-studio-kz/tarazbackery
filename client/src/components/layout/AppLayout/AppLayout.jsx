a1700b5a9391974d1f70a7e9152fcb0a1e3d2c89
web-studio-kz <bypcpotap@gmail.com>
2025-08-13 17:19:13 +0500

    fix: mobile first

--- client/src/components/layout/AppLayout/AppLayout.jsx
+++ client/src/components/layout/AppLayout/AppLayout.jsx
@@ -1,31 +1,18 @@
-// src/components/layout/AppLayout/AppLayout.jsx (ФИНАЛЬНАЯ, ПРАВИЛЬНАЯ ВЕРСИЯ)
-
 import React from 'react';
 import styles from './AppLayout.module.css';
 import Header from '../Header/Header';
-import BottomNav from '../BottomNav/BottomNav';
+import BottomNav from '../BottomNav/BottomNav'
 import Footer from '../Footer/Footer';
 
-import { useMatchMedia } from '../../../hooks/useMatchMedia';
 
 const AppLayout = ({ children }) => {
-  const { isMobile } = useMatchMedia();
-
-  // Пока мы не определили тип устройства (isMobile === null),
-  // мы не рендерим ничего. Это предотвратит "моргание"
-  // и гарантирует, что мы покажем сразу правильный интерфейс.
-  if (isMobile === null) {
-    return null; // Или можно вернуть <Spinner />, но null быстрее
-  }
-
   return (
     <div className={styles.wrapper}>
-      {isMobile ? <BottomNav /> : <Header />}
-      
+      <Header />
       <main className={styles.container}>
         {children}
       </main>
-      
+      <BottomNav />
       <Footer />
     </div>
   );
