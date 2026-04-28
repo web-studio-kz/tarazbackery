ff2f366ee77b146c822b8b331a92cbf8dc5cfd39
web-studio-kz <bypcpotap@gmail.com>
2026-04-28 17:09:16 +0500

    fixt:

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -3,6 +3,16 @@ import ProductItem from '../ProductItem/ProductItem';
 import styles from './ProductList.module.css'; 
 
 const ProductList = ({ products }) => {
+    if (!products || products.length === 0) {
+        return (
+            <div className={styles.list}>
+                {[...Array(8)].map((_, index) => (
+                    <div key={index} className={styles.skeletonCard}></div>
+                ))}
+            </div>
+        );
+    }
+
     return (
         <div className={styles.list}>
             {products.map((product, index) => (
