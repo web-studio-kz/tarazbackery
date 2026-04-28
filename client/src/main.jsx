67c6af667178f7124a89d11187c985601f6b9710
web-studio-kz <bypcpotap@gmail.com>
2026-04-28 15:46:29 +0500

    feat: add npm fonts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,7 @@
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
