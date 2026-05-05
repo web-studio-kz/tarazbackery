d887f897f9b42e142aa63c134c2743add25a82ba
web-studio-kz <bypcpotap@gmail.com>
2026-05-05 21:33:46 +0500

    fix: sending mail

--- server/services/mailService.js
+++ server/services/mailService.js
@@ -7,11 +7,15 @@ class MailService {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: false,
+            secure: true,
+            pool: true, 
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD,
             },
+            tls: {
+                rejectUnauthorized: false
+            }
         });
 
         this.sendNewOrderNotification = this.sendNewOrderNotification.bind(this);
